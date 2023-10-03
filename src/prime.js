529a59b480a0d4a220ad52142c4608c00b0f9672
tan1ty <tatiana.kovalenko1992@gmail.com>
2023-10-03 19:24:53 +0200

    prime problem solution

---
+++ src/prime.js
@@ -0,0 +1,27 @@
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  } else {
+    if (num === 2) {
+      return true;
+    }
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function showPrime(min, max) {
+  let primes = [];
+  for (let i = min; i <= max; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+showPrime(1, 100);
