7c90772eafc72cfc490c0529b250bf5216023276
tan1ty <tatiana.kovalenko1992@gmail.com>
2023-10-15 18:42:24 +0200

    refactor: countPoint feature

--- src/scrabbleArr.js
+++ src/scrabbleArr.js
@@ -3,34 +3,7 @@ function getWordFromPlayer(playerNumber) {
 }
 
 function countPoints(player) {
-  const letters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-  ];
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   const points = [
     1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4,
     10,
