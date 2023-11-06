05b1acea9f297a4bfac4abf9af9a757d6217bb8d
tan1ty <tatiana.kovalenko1992@gmail.com>
2023-11-06 19:05:00 +0100

    bank problem solution

---
+++ src/bank.py
@@ -0,0 +1,8 @@
+def get_greeting():
+    greeting = input("Type your greeting: ")
+    greeting = greeting.strip().lower()
+    if "hello" in greeting:
+       print("$0")
+    elif greeting[0] == "h" and greeting[1] != "e":
+       print("$20")
+    else: print("$100")
